3b3a97021f9cede22d41eec6bf4a5dc53e60b849
Azicel <62797469+Azicel@users.noreply.github.com>
2023-01-14 06:06:31 +0500

    Подключил boostrap и сделал миграцию по страницам

--- analyst/urls.py
+++ analyst/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.main),
+    path('main.html',views.main),
+    path('years.html',views.years),
+    path('cities.html',views.cities),
 ]
--- analyst/views.py
+++ analyst/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def main(requset):
     return render(requset, 'analyst/main.html')
+
+def years(requset):
+    return render(requset, 'analyst/years.html')
+
+def cities(request):
+    return render(request,'analyst/cities.html')
